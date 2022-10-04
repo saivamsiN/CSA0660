7497445e2e3a132d1cf9c83e32ac814d63862a91
saivamsiN <113550891+saivamsiN@users.noreply.github.com>
2022-10-04 06:20:19 +0100

    Add files via upload

---
+++ number pattern 2.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, k, N;
+
+    printf("Enter N: ");
+    scanf("%d", &N);
+
+    for(i=1; i<=N; i++)
+    {
+        k = i;
+        for(j=1; j<=i; j++, k++)
+        {
+            printf("%d", k);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
